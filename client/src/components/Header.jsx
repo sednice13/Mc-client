ab70338bf1076025bf9e4bb241ed19ec97544c9c
Sednice13 <sa224pb@student.lnu.se>
2023-04-05 14:40:08 +0200

    making a started firstversion of header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,13 +7,16 @@ const Header = () => {
 
     return (
         
-        <Navbar bg="dark" variant="dark">
-          <Container>
+        <Navbar bg="dark" variant="dark" style={{height: '100px'}}>
+          <Container fluid>
             <Navbar.Brand href="#home">Navbar</Navbar.Brand>
             <Nav className="me-auto">
               <Nav.Link href="#home">Home</Nav.Link>
               <Nav.Link href="#features">Features</Nav.Link>
               <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              
             </Nav>
           </Container>
         </Navbar>
