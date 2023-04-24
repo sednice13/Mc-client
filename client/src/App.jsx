9926330fe58453fa21887fc097df62fc8c3fa8cc
Sednice13 <sa224pb@student.lnu.se>
2023-04-24 16:30:23 +0200

    adding own created register compenet i have made in other project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
+import  {BrowserRouter as Router,  Routes, Route }   from 'react-router-dom'
 import React, { Component}from 'react';
 import Header from './components/base/Header';
 import Footer from './components/base/Footer'
+
+import Signin from './components/account/Signin';
 
 
 class App extends Component {
@@ -11,6 +14,18 @@ class App extends Component {
     return (
       <div>
         <Header/>
+        <Router>
+
+          <Routes>
+       
+          <Route  path='/create-account' exact element={<Signin/>} />
+
+          </Routes>
+        </Router>
+        <div>
+        <h5 > <a href='/create-account'> Registera</a></h5>
+            <h5> Logga in</h5>
+        </div>
         <Footer/>
       
       </div>
---
+++ client/src/components/account/Signin.jsx
@@ -0,0 +1,137 @@
+import React from "react";
+import mystyles from './styles/mystyles.module.css'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
+const Signin = () => {
+
+   const navigate = useNavigate()
+
+   const loginNavigate = () => {
+
+
+      navigate('/login')
+
+   }
+
+
+   const [formValue, setformValue] = React.useState({
+      username: '',
+      password: '',
+      mail: ''
+
+   });
+
+   const [error, setError] = React.useState({
+
+      errortext: null
+   })
+
+   const handleSubmit = async (e) => {
+
+
+      e.preventDefault();
+
+
+
+      try {
+
+         console.log('testt')
+         const registerReq = await axios.post('/user/signup', {
+
+            user: formValue.username,
+            password: formValue.password,
+            mail: formValue.mail
+
+
+         })
+
+         if (registerReq.status === 200) {
+
+            loginNavigate()
+         }
+
+
+
+      } catch (error) {
+
+         if (error.response.status === 400) {
+
+            setError({
+
+               errortext: error.response.data.error
+            })
+
+         }
+
+       
+      }
+    
+
+   }
+
+   const handleChange = (event) => {
+      event.preventDefault();
+      setformValue({
+         ...formValue,
+         [event.target.name]: event.target.value,
+      });
+
+
+   }
+   return (
+
+      <div className={mystyles.fulldivcontent}>
+         <div className={mystyles.divsize}>
+
+            <h1 className={mystyles.thetitle}> SIGN IN</h1>
+
+            <form className={mystyles.signinform} onSubmit={handleSubmit} >
+
+
+
+               <label className={mystyles.labels}>
+                  Username
+               </label>
+
+               <input type='text' className={mystyles.inputs} value={formValue.username} name='username' onChange={handleChange}>
+
+               </input>
+
+
+               <label className={mystyles.labels}>
+                  Mail Adress
+               </label>
+
+               <input type='text' className={mystyles.inputs} name="mail" onChange={handleChange}>
+
+               </input>
+
+               <label className={mystyles.labels}>
+                  Password
+               </label>
+
+
+
+               <input type='password' className={mystyles.inputs} name="password" onChange={handleChange}>
+
+               </input>
+               <p className={error.errortext !== null ? mystyles.error : mystyles.noerror} > {error.errortext} </p>
+               <button className={mystyles.coonectbutton}>Sign in </button>
+
+               <p> Terms of Service</p>
+
+            </form>
+
+         </div>
+      </div>
+   )
+
+
+}
+
+
+
+
+
+export default Signin
